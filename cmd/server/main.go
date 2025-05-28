55afa50fe622ad5b23f98f4c6d9a207c2be27de6
Will Roberts <willroberts3@protonmail.com>
2025-05-28 15:47:22 -0400

    Adds Alt-Svc header

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Alt-Svc", "h3=\":8000\"; ma=86400")
 		w.WriteHeader(http.StatusOK)
 	})
 	if err := http3.ListenAndServeQUIC("0.0.0.0:8000", "/etc/certs/ssl.crt", "/etc/certs/ssl.key", mux); err != nil {
